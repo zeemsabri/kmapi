f049b6a66c8105be79fc71b20dbd447aee66a507
zeemsabri <70251412+zeemsabri@users.noreply.github.com>
2025-10-30 09:57:49 +0800

    Update Stripe API version to `2025-10-29.clover`

--- src/services/stripeService.ts
+++ src/services/stripeService.ts
@@ -5,7 +5,7 @@ if (!process.env.STRIPE_SECRET_KEY) {
 }
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY, {
-  apiVersion: '2025-09-30.clover'
+  apiVersion: '2025-10-29.clover'
 });
 
 export class StripeService {
